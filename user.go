c41f529cbea2c0e28819ccf047223bec4c0cc83c
Neeraj.Kumar <neeraj.kumar@acronis.com>
2017-05-16 17:45:00 +0800

    changes in progress

--- user.go
+++ user.go
@@ -1,18 +1,41 @@
 package usermgmt
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// User ...
 type User struct {
-	Id   int
-	Name string
+	Id       int
+	Name     string
+	Password string
 }
 
-func Register(user User) bool {
-
+// RegisterHandler ...
+func (env *Env) RegisterHandler(w http.ResponseWriter, r *http.Request) bool {
+	return true
 }
 
-func Login(user User) bool {
+// LoginHandler ...
+func (env *Env) LoginHandler(w http.ResponseWriter, r *http.Request) bool {
+	var user User
 
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return false
+	}
+
+	return Authenticate(user)
 }
 
-func Logout(user User) bool {
+// LogoutHandler ...
+func (env *Env) LogoutHandler(w http.ResponseWriter, r *http.Request) bool {
+	return true
+}
 
+// Authenticate ...
+func Authenticate(user User) bool {
+	return user.Name == "admin" && user.Password == "pwd"
 }
