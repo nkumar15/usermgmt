0e05febcc68c548f80a4de378423bfefc81e5807
Neeraj.Kumar <neeraj.kumar@acronis.com>
2017-08-03 12:34:27 +0800

    final version for review

--- routes.go
+++ routes.go
@@ -1,20 +1,5 @@
 package usermgmt
 
-import (
-	"net/http"
-)
-
-// Route ...
-type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
-}
-
-// Routes ...
-type Routes []Route
-
 const (
 	AddUserRoute    = "/user"
 	GetUserRoute    = "/user/{Id}"
